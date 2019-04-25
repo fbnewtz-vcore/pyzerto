88bf4abfa7370ef60c80d54747d9afa50a4f7441
Fred Newtz <frederick.newtz@vcoretec.com>
2019-04-25 10:24:13 -0700

    Update datastore.py

--- zerto/datastore.py
+++ zerto/datastore.py
@@ -10,15 +10,15 @@ class Datastore(ZertoObject):
         self.values = kwargs
         self.name = kwargs['DatastoreName']
         self.identifier = kwargs['DatastoreIdentifier']
-        self.type = kwargs.get('Type')
+#        self.type = kwargs.get('Type')
 #        self.capacityinbytes = kwargs.get('CapactiyInBytes')
 #        self.freeinbytes = kwargs.get('FreeInBytes')
 #        self.provisionedinbytes = kwargs.get('ProvisionedInBytes')
 #        self.usedinbytes = kwargs.get('UsedInBytes')
 
     def __str__(self):
-        return 'name={0}, identifier={1}, type={2}'.format(
-            self.name, self.identifier, self.type)
+        return 'name={0}, identifier={1}'.format(
+            self.name, self.identifier)
 
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
