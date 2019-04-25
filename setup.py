a0e82e2eed58da5030bd4fa4afc18da3341d4f98
Fred Newtz <frederick.newtz@vcoretec.com>
2019-04-25 09:16:46 -0700

    Updated datastore.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def read(fname):
 
 setup(
     name='Zerto',
-    version='0.4dev',
+    version='0.5dev',
     description='Module for calling the Zerto api',
     author='Kees Bos',
     author_email='k.bos@capitar.com',
