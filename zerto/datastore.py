104aebeddb96af241099b6a6c196d7f68decc4ad
Fred Newtz <frederick.newtz@vcoretec.com>
2019-04-26 17:00:13 -0700

    vsvms

--- zerto/datastore.py
+++ zerto/datastore.py
@@ -10,11 +10,6 @@ class Datastore(ZertoObject):
         self.values = kwargs
         self.name = kwargs['DatastoreName']
         self.identifier = kwargs['DatastoreIdentifier']
-#        self.type = kwargs.get('Type')
-#        self.capacityinbytes = kwargs.get('CapactiyInBytes')
-#        self.freeinbytes = kwargs.get('FreeInBytes')
-#        self.provisionedinbytes = kwargs.get('ProvisionedInBytes')
-#        self.usedinbytes = kwargs.get('UsedInBytes')
 
     def __str__(self):
         return 'name={0}, identifier={1}'.format(
