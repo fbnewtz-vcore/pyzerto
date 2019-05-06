62bb396420cc4da265611e1e62f910e75b346576
Fred Newtz <frederick.newtz@vcoretec.com>
2019-05-06 12:38:09 -0700

    Updated VPG substatus

--- zerto/vpg.py
+++ zerto/vpg.py
@@ -12,7 +12,7 @@ class VPG(ZertoObject):
         self.name = kwargs['VpgName']
         self.identifier = kwargs['VpgIdentifier']
         self.status = vpg_status[kwargs['Status']]
-        self.sub_status = vpg_sub_status[kwargs['SubStatus']]
+#        self.sub_status = vpg_sub_status[kwargs['SubStatus']]
         self.priority = kwargs.get('Priority')
         self.source_site = kwargs.get('SourceSite')
         self.target_site = kwargs.get('TargetSite')
@@ -23,8 +23,8 @@ class VPG(ZertoObject):
         self.used_storage = kwargs.get('UsedStorageInMB')
 
     def __str__(self):
-        return 'name={0}, identifier={1}, status={2}, sub_status={3}'.format(
-            self.name, self.identifier, self.status, self.sub_status)
+        return 'name={0}, identifier={1}, status={2}'.format(
+            self.name, self.identifier, self.status)
 
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
